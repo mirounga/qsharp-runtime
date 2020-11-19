60ee0dac8880fdb046b80aed9953ff870f29ba1b
Irina Yatsenko <irinayat@microsoft.com>
2020-11-18 23:29:22 -0800

    Try setting compiler via environtment variable

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -3,14 +3,18 @@
 
 Write-Host "##[info]Build Native simulator"
 
-$compiler = ""
+$oldCC = $env:CC
+$oldCXX = $env:CXX
+
 if (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin"))))
 {
-        $compiler = "-DCMAKE_C_COMPILER= gcc-7 -DCMAKE_CXX_COMPILER= g++-7 "
+    $env:CC = "/usr/bin/gcc-7"
+    $env:CXX = "/usr/bin/g++-7"
 }
 elseif (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin"))))
 {
-    $compiler = "-DCMAKE_C_COMPILER= gcc -DCMAKE_CXX_COMPILER= g++"
+    $env:CC = "/usr/bin/gcc"
+    $env:CXX = "/usr/bin/g++"
 }
 elseif (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Win"))))
 {
@@ -25,11 +29,13 @@ if (-not (Test-Path $nativeBuild)) {
 }
 Push-Location $nativeBuild
 
-# We never build debug build of native simulator? (-DCMAKE_BUILD_TYPE= $BuildConfiguration)
-cmake $compiler -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE= $Env:BUILD_CONFIGURATION ../..
+cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE= $Env:BUILD_CONFIGURATION ../..
 cmake --build .
 
 Pop-Location
+
+$env:CC = $oldCC
+$env:CXX = $oldCXX
 
 if ($LastExitCode -ne 0) {
     Write-Host "##vso[task.logissue type=error;]Failed to build Native simulator."
